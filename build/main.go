39d7192cad32e66a5e6c47a336e3d440ca731160
Or Elimelech <vic3lord@users.noreply.github.com>
2024-10-16 17:31:39 +0300

    chore: dagger CI (#1)

---
+++ build/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+var task = flag.String("task", "", "task to run")
+
+func Test(ctr *dagger.Container) *dagger.Container {
+	return ctr.WithExec([]string{"go", "test", "-v", "./..."})
+}
+
+func Build(ctr *dagger.Container) *dagger.Container {
+	return ctr.WithExec([]string{"go", "build", "."})
+}
+
+func All(ctr *dagger.Container) *dagger.Container {
+	return ctr.
+		With(Test).
+		With(Build)
+}
+
+func Task(name string) func(ctr *dagger.Container) *dagger.Container {
+	switch name {
+	case "test":
+		return Test
+	case "build":
+		return Build
+	}
+	return All
+}
+
+func Publish(dag *dagger.Client, ctr *dagger.Container) *dagger.Container {
+	return dag.Container().
+		From("gcr.io/distroless/static").
+		WithFile("/bufile", ctr.File("/src/bufile")).
+		WithEntrypoint([]string{"/bufile"})
+}
+
+func Base(dag *dagger.Client) *dagger.Container {
+	return dag.Container().
+		From("golang:1.23").
+		WithMountedDirectory("/src", dag.Host().Directory(".")).
+		WithWorkdir("/src")
+}
+
+func main() {
+	flag.Parse()
+	ctx := context.Background()
+	dag, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		slog.Error("Connect dagger engine", slog.String("err", err.Error()))
+		os.Exit(1)
+	}
+
+	ctr := Base(dag).
+		With(Task(*task))
+
+	if *task != "" {
+		slog.Info("[Dev mode] Image won't be published", slog.String("task", *task))
+		return
+	}
+
+	_, err = Publish(dag, ctr).Sync(ctx)
+	if err != nil {
+		slog.Error("Publish container", slog.String("err", err.Error()))
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,17 +3,13 @@ module github.com/vic3lord/bufile
 go 1.23.2
 
 require (
+	buf.build/gen/go/bufbuild/reflect/connectrpc/go v1.17.0-20240117202343-bf8f65e8876c.1
+	buf.build/gen/go/bufbuild/reflect/protocolbuffers/go v1.35.1-20240117202343-bf8f65e8876c.1
 	connectrpc.com/connect v1.17.0
-	github.com/bufbuild/buf v1.45.0
-	google.golang.org/grpc v1.67.1
+	google.golang.org/protobuf v1.35.1
 )
 
 require (
-	buf.build/gen/go/bufbuild/reflect/connectrpc/go v1.17.0-20240117202343-bf8f65e8876c.1 // indirect
-	buf.build/gen/go/bufbuild/reflect/protocolbuffers/go v1.35.1-20240117202343-bf8f65e8876c.1 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240930140551-af27646dc61f // indirect
-	google.golang.org/protobuf v1.35.1 // indirect
 )
